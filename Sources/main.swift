2cd756b52d054352825deaeb2cea843903d9517c
Kojirou <kojiroujyy@vip.qq.com>
2016-07-20 14:44:30 +0800

    update

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,5 +1,39 @@
 import PerfectHTTPServer
+import PerfectLib
+import PerfectHTTP
 import SFMongo
 import MongoDB
+
+let server = HTTPServer()
+
+var routes = Routes()
+
+routes.add(method: .get, uri: "/") { (request, response) in
+    response.appendBody(string: "Hello World!")
+    response.completed()
+}
+
+routes.add(method: .get, uri: "/log/{id}") { (request, response) in
+    if let logId = request.urlVariables["id"] {
+        print("Log ID: \(logId)")
+    }else {
+        print("No Log ID")
+    }
+    response.setHeader(HTTPResponseHeader.Name.contentType, value: "application/json")
+    response.setBody(string: "{\"data\": 123}")
+    response.completed()
+}
+
+server.addRoutes(routes)
+
+server.serverPort = 8181
+
+server.documentRoot = "./webroot"
+
+do {
+    try server.start()
+} catch PerfectError.networkError(let err, let msg) {
+    print("Network error thrown: \(err) \(msg)")
+}
 
 
