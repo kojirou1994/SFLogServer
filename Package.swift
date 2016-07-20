6410b0016d949ea365076b01b533933d35401417
Kojirou <kojiroujyy@vip.qq.com>
2016-07-20 16:03:11 +0800

    update Package Description

--- Package.swift
+++ Package.swift
@@ -2,6 +2,13 @@ import PackageDescription
 
 let package = Package(
     name: "SFLogServer",
+    targets: [
+        Target(name: "MainServer", dependencies: [.Target(name: "Helpers")]),
+        Target(
+            name: "Helpers",
+            dependencies: [.Target(name: "Models")]),
+        Target(name: "Models")
+    ],
     dependencies: [
         .Package(url: "http://git.sfdai.com/Kojirou1994/SFMongo.git", versions: Version(0,0,0)..<Version(10,0,0)),
         .Package(url:"https://github.com/PerfectlySoft/Perfect-HTTPServer.git", versions: Version(0,0,0)..<Version(10,0,0)),
