4d60c6c1f78550a88f0d48ebe317dad881067ef4
Sean <694343149@qq.com>
2016-07-21 14:00:27 +0800

    modify app model

--- Sources/Models/v1/App.swift
+++ Sources/Models/v1/App.swift
@@ -11,34 +11,37 @@ import SFMongo
 
 public struct App: SFModel {
     var _id: ObjectId
-    
-    var info: String
-    
-    var key: String
-    
-    var version: String
-    
-    var state: AppState
-    
+    //app key
+    var app_key: String
+    //app 名称
     var name: String
+    //app 介绍网址
+    var description_url:String?
+    //创建时间
+    var create_time:Date
+    //更新时间
+    var update_time:Date?
+    //是否冻结
+    var disabled:Bool
+    //是否删除
+    var deleted:Bool
+    //版本
+    var version: String
+
     
     public init(json: JSON) throws {
-        guard let id = json["_id"].oid, info = json["info"].string, key = json["key"].string, version = json["version"].string, state = AppState(rawValue: json["state"].intValue), name = json["name"].string  else {
+        guard let id = json["_id"].oid,key = json["key"].string, version = json["version"].string, name = json["name"].string,disabled = json["disabled"].bool,deleted = json["delected"].bool, create_time = json["create_time"].date else {
             throw SFMongoError.invalidData
         }
         
         self._id = id
-        self.info = info
-        self.key = key
-        self.version = version
-        self.state = state
+        self.app_key = key
         self.name = name
-        
+        self.description_url = json["description_url"].string
+        self.create_time = create_time
+        self.update_time = json["update_time"].date
+        self.disabled = disabled
+        self.deleted = deleted
+        self.version = version
     }
-    
-}
-
-enum AppState: Int {
-    case closed = 0
-    case open = 1
 }
