5d47d636a96078757998437d41c8318c69f66337
Kojirou <kojiroujyy@vip.qq.com>
2016-07-21 10:10:09 +0800

    add init for Log

--- Sources/Models/v1/Log.swift
+++ Sources/Models/v1/Log.swift
@@ -10,14 +10,14 @@ import Foundation
 import SFMongo
 
 
-enum LogSource: Int {
+public enum LogSource: Int {
     case web = 0
     case iOS = 1
     case android = 2
     case other = 3
 }
 
-enum LogLevel: Int {
+public enum LogLevel: Int {
     case info
     case error
     case debug
@@ -25,37 +25,52 @@ enum LogLevel: Int {
 }
 
 public struct Log: SFModel {
+    /// ID
     var _id: ObjectId
-    
+    /// APP IP
     var appId: String
-    
+    /// 状态
     var state: String
-    
+    /// 日志来源
     var source: LogSource
-    
+    /// 用户UA
     var userAgent: String
-    
+    /// 发送设备
     var device: String
-    
+    /// 来源IP
     var sourceIP: String
-    
+    /// 来源用户id
     var sourceUserId: String
-    
+    /// 来源用户名
     var sourceUsername: String
-    
+    /// 日志等级
     var level: LogLevel
-    
+    /// 日志内容
     var content: String
-    
+    /// 创建时间
     var createTime: Date
     
-    //var logDescription:LogDescription
     
     public init(json: JSON) throws {
         guard let id = json["_id"].oid, appId = json["appId"].string, state = json["state"].string, source = LogSource(rawValue: json["source"].intValue), userAgent = json["userAgent"].string, device = json["device"].string, sourceIP = json["sourceIP"].string, sourceUserId = json["sourceUserId"].string, sourceUsername = json["sourceUsername"].string, level = LogLevel(rawValue: json["level"].intValue), content = json["content"].string, createTime = json["createTime"].date  else {
             throw SFMongoError.invalidData
         }
         self._id = id
+        self.appId = appId
+        self.state = state
+        self.source = source
+        self.userAgent = userAgent
+        self.device = device
+        self.sourceIP = sourceIP
+        self.sourceUserId = sourceUserId
+        self.sourceUsername = sourceUsername
+        self.level = level
+        self.content = content
+        self.createTime = createTime
+    }
+    
+    public init(appId: String, state: String, source: LogSource, userAgent: String, device: String, sourceIP: String, sourceUserId: String, sourceUsername: String, level: LogLevel, content: String, createTime: Date) {
+        self._id = ObjectId.generate()
         self.appId = appId
         self.state = state
         self.source = source
