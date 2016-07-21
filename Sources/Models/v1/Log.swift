a0d046cd3d136fec2b61d8c792715a7cccbdfc5b
Kojirou <kojiroujyy@vip.qq.com>
2016-07-21 14:19:42 +0800

    fix log

--- Sources/Models/v1/Log.swift
+++ Sources/Models/v1/Log.swift
@@ -45,20 +45,18 @@ public struct Log: SFModel {
     //日志等级
     var level: LogLevel
     //日志内容
-    var content: Any
+    var content: String
     //创建时间
     var create_time: Date
     //是否删除
     var deleted:Bool
-    //log描述
-    //var logDescription:LogDescription
     
     public init(json: JSON) throws {
-        guard let id = json["_id"].oid, app_info = json["app"].string, source = LogSource(rawValue: json["source"].intValue),source_ip = json["source_ip"].string,level = LogLevel(rawValue: json["level"].intValue), content = json["content"].arrayObject, create_time = json["createTime"].date,deleted = json["deleted"].bool  else {
+        guard let id = json["_id"].oid, app = try? App(json: json["app"]), source = LogSource(rawValue: json["source"].intValue), source_ip = json["source_ip"].string, level = LogLevel(rawValue: json["level"].intValue), content = json["content"].string, create_time = json["createTime"].date, deleted = json["deleted"].bool else {
             throw SFMongoError.invalidData
         }
         self._id = id
-        self.app = try App(json:JSON(app_info))
+        self.app = app
         self.source = source
         self.source_ip = source_ip
         self.device_description = json["device_description"].string
@@ -70,31 +68,29 @@ public struct Log: SFModel {
         self.deleted = deleted
     }
     
-//    public init?(request: HTTPRequest) {
-//        var dic = [String: String]()
-//        for (key, value) in request.postParams {
-//            dic[key] = value
-//        }
-//        
-//        let json = JSON(dic)
-//        
-//        guard let appId = json["appId"].string, state = json["state"].string, source = LogSource(rawValue: json["source"].intValue), userAgent = json["userAgent"].string, device = json["device"].string, sourceIP = json["sourceIP"].string, sourceUserId = json["sourceUserId"].string, sourceUsername = json["sourceUsername"].string, level = LogLevel(rawValue: json["level"].intValue), content = json["content"].string  else {
-//            return nil
-//        }
-//        
-//        self._id = ObjectId.generate()
-//        self.appId = appId
-//        self.state = state
-//        self.source = source
-//        self.userAgent = userAgent
-//        self.device = device
-//        self.sourceIP = sourceIP
-//        self.sourceUserId = sourceUserId
-//        self.sourceUsername = sourceUsername
-//        self.level = level
-//        self.content = content
-//        self.createTime = Date()
-//    }
+    public init?(request: HTTPRequest) {
+        var dic = [String: String]()
+        for (key, value) in request.postParams {
+            dic[key] = value
+        }
+        
+        let json = JSON(dic)
+        
+        guard let app = try? App(json: json["app"]), source = LogSource(rawValue: json["source"].intValue), level = LogLevel(rawValue: json["level"].intValue), content = json["content"].string else {
+            return nil
+        }
+        self._id = ObjectId.generate()
+        self.app = app
+        self.source = source
+        self.source_ip = request.remoteAddress.host
+        self.device_description = json["device_description"].string
+        self.device_token = json["device_token"].string
+        self.title = json["title"].string
+        self.level = level
+        self.content = content
+        self.create_time = Date()
+        self.deleted = false
+    }
 }
 
 
