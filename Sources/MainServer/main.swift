c9e551c36c88380098731841079170b465254b7e
Kojirou <kojiroujyy@vip.qq.com>
2016-07-25 13:37:51 +0800

    remove login submit

--- Sources/MainServer/main.swift
+++ Sources/MainServer/main.swift
@@ -15,25 +15,6 @@ routes.add(method: .get, uri: "/") { (request, response) in
     response.completed()
 }
 
-routes.add(method: .post, uri: "/login") { (req, res) in
-    guard let appId = req.param(name: "appId"), appKey = req.param(name: "appKey") else {
-        res.status = .badRequest
-        res.completed()
-        return
-    }
-    print(appId)
-    print(appKey)
-    res.completed()
-}
-
-//提交日志
-routes.add(method:.post,uri: "/submit"){ (request,response) in
-    guard let access_token = request.param(name:"access_token") else{
-        response.status = .badRequest
-        response.completed()
-        return
-    }
-}
 
 routes.add(method: .get, uri: "/log") { (request, response) in
     response.setHeader(HTTPResponseHeader.Name.contentType, value: "application/json")
@@ -43,11 +24,11 @@ routes.add(method: .get, uri: "/log") { (request, response) in
 
 routes.add(method: .get, uri: "/log/{id}") { (request, response) in
     if let logId = request.urlVariables["id"] {
-        print("Log ID: \(logId)")
+        var logs = LogDBManager.shared.findLog(byLogId: logId)
         response.setHeader(HTTPResponseHeader.Name.contentType, value: "application/json")
-        response.setBody(string: LogDBManager.shared.findLog(byLogId: logId)?.jsonString ?? "")
+        response.setBody(string: logs?.jsonString ?? "")
+        logs = nil
     }else {
-        print("No Log ID")
         response.status = .notFound
     }
     
